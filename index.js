013f97f4763914bb403e90c521564220ae0af8b6
joanrig <github email address>
2019-05-24 20:47:04 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers =
+function returnFirstTwoDrivers(drivers) {
+  let newDrivers = drivers
+  newDrivers.length = 2
+  return newDrivers
+}
+
+const returnLastTwoDrivers =
+function returnLastTwoDrivers(drivers) {
+  let newDrivers = drivers
+  newDrivers.slice(-2)
+  return newDrivers
+}
